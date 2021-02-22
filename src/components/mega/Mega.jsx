9f74d80785014e0f50c7112fc3b6dee1e41ae144
gabones <gabones@teste.com>
2021-02-22 00:16:26 -0300

    final

--- src/components/mega/Mega.jsx
+++ src/components/mega/Mega.jsx
@@ -1,33 +1,26 @@
 import React, {useState} from 'react'
-import Display from '../contador/Display'
 
 export default props => {
 
-    let [jogos, setJogos] = useState('6')
-
-    function naMudança(e) {
-        if (e.target.value >= 6) {
-            setJogos(e.target.value)
-        }
-    }
+    const [qtde, setQtde] = useState(props.qtde || 6)
+    const [jogos, setJogos] = useState(Array(props.qtde || 6).fill(0))
 
     function geraNumeros() {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < jogos; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
+        let numbers = [];
+        let aux = 0;
+        for (let i = 0; numbers.length < qtde; i++) {
+          aux = Math.round(Math.random() * (60 - 1) + 1)
+          numbers.includes(aux) ? aux = 0 : numbers.push(aux)
         }
-        return color;
+        return numbers.sort((a,b) => a-b);
     }
 
     return(
     <div>
-        <div>MegaSena</div>
-        <input id="nrojogo" type="number" value={jogos} onChange={naMudança}/>
-        <Display numero={jogos}></Display>
-        <div>
-            <button onClick={geraNumeros}>Gerar Jogo</button>
-        </div>
+        <h2>Mega</h2>
+        <input type="number" min="6" value={qtde} onChange={e => setQtde(+e.target.value)}/>
+        <h3>{jogos.join(' ')}</h3>
+        <button onClick={_ => setJogos(geraNumeros(qtde))}>Gerar Números</button>
     </div>
     )
 }
